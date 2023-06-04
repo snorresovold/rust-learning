16112530af12eee5656329f7cb5a7f6dca668877
Snorre Søvold <snorresovold@gmail.com>
2023-06-04 18:42:06 +0200

    updates

--- rust_api/src/main.rs
+++ rust_api/src/main.rs
@@ -1,3 +1,14 @@
-fn main() {
-    println!("Hello, world!");
+#[macro_use]
+extern crate rocket;
+
+#[get("/")]
+fn index() -> String {
+    String::from("Hello world")
+}
+
+
+#[launch]
+fn rocket() -> _ {
+    rocket::build()
+        .mount("/", routes![index])
 }
